d401e234a78e989d315bada10e037429e9a23872
Abu Sadat Ansari <itssadatansari@gmail.com>
2024-10-06 14:43:57 +0530

    Hooks and Methods in mongoose

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 const userSchema = new Schema(
     {
@@ -46,5 +48,26 @@ const userSchema = new Schema(
     }, 
     { timestamps: true } // Automatically adds createdAt and updatedAt
 )
+
+userSchema.pre("save", async function (next) {
+
+    if(!this.modified("password")) return next()
+    
+    this.password = bcrypt.hash(this.password, 10)
+
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password){
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function (){
+    // short lived access token
+    return jwt.sign({
+        _id: this._id,
+        username: this.username
+    }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: process.env.ACCESS_TOKEN_EXPIRY})
+}
 
 export const User = mongoose.model("User", userSchema) 
